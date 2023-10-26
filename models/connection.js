e5b9b1bf40ed8100f20f451d7faf05b21e9b42b1
devandresilva <andresilva.kat@gmail.com>
2023-10-25 21:24:54 -0300

    fix: host sequelize

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize('pokemons', 'pokemon', 'pokemon', {
-  host: 'bd',
+  host: '172.16.0.12',
   dialect: 'postgres',
   port: 5432
 });
